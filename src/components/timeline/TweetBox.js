53154f216c33cdbafc274c5b7f9dfcd20bb1f69f
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-11 16:28:09 +0900

    TweetBoxコンポーネントを作成

---
+++ src/components/timeline/TweetBox.js
@@ -0,0 +1,23 @@
+import { Avatar, Button } from "@mui/material";
+import React from "react";
+
+const TweetBox = () => {
+  return (
+    <div className="tweetBox">
+      <form>
+        <Avatar />
+        <input placeholder="いまどうしてる？" type="text"></input>
+        <input
+          className="tweetBox--imageInput"
+          placeholder="画像のURLを入力してください"
+          type="text"
+        ></input>
+        <Button className="tweetBox--tweetButton" type="submit">
+          ツイートする
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default TweetBox;
