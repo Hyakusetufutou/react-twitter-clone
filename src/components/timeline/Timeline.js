db16d583873a59c0d82372b2247023654b5cbce3
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-11 16:07:06 +0900

    タイムラインのコンポーネントを作成

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Timeline = () => {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline--header">
+        <h2>ホーム</h2>
+      </div>
+
+      {/* TweetBox */}
+
+      {/* Post */}
+    </div>
+  );
+};
+
+export default Timeline;
