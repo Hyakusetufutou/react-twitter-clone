a4e1a3d01757d528b6b8977d1c22dc9b030b5e18
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-11 13:36:01 +0900

    Material UIを導入

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import XIcon from "@mui/icons-material/X";
 
 const Sidebar = () => {
   return (
     <div>
       {/* ツイッターアイコン */}
+      <XIcon className="sidebar--twitterIcon" />
 
       {/* SidebarOption */}
       {/* SidebarOption */}
