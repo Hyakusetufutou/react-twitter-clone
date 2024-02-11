e7aa65b14d535ba3c0e101069fb07b961f1cc09f
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-11 14:04:01 +0900

    SidebarOptionを実装

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,14 @@
 import React from "react";
 import XIcon from "@mui/icons-material/X";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
+import ListAltOutlinedIcon from "@mui/icons-material/ListAltOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Sidebar = () => {
   return (
@@ -8,10 +17,14 @@ const Sidebar = () => {
       <XIcon className="sidebar--twitterIcon" />
 
       {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption text="ホーム" Icon={HomeIcon} />
+      <SidebarOption text="話題を検索" Icon={SearchIcon} />
+      <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+      <SidebarOption text="ブックマーク" Icon={TurnedInNotIcon} />
+      <SidebarOption text="リスト" Icon={ListAltOutlinedIcon} />
+      <SidebarOption text="プロフィール" Icon={PersonOutlinedIcon} />
+      <SidebarOption text="もっとみる" Icon={MoreHorizIcon} />
       {/* SidebarOption */}
 
       {/* ツイートボタン */}
