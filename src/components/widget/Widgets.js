a0337396eabe6ce98a7b34232a82a5d80c67d4bc
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-11 21:37:53 +0900

    タイムラインを表示するよう変更

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,5 +1,17 @@
 import { Search } from "@mui/icons-material";
 import React from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
@@ -11,6 +23,19 @@ function Widgets() {
 
       <div className="widgets--widgetContainer">
         <h2>いまどうしてる？</h2>
+
+        <TwitterTweetEmbed tweetId={"1750836515847151641"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="SAIONJI_com"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"http://twitter.com/Shin_Engineer"}
+          options={{ text: "#React.js勉強中", via: "Shin_Engineer" }}
+        />
       </div>
     </div>
   );
