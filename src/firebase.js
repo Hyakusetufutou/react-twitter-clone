4d2aaec5931e6779cd5fbb04804d7be3b994d155
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-02-12 10:22:58 +0900

    firebaseの初期化設定を実装

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARe6z2Ia1rJftR6dJXzjdx1GZc5Fgp2WE",
+  authDomain: "twitter-clone-udemy-e59e0.firebaseapp.com",
+  projectId: "twitter-clone-udemy-e59e0",
+  storageBucket: "twitter-clone-udemy-e59e0.appspot.com",
+  messagingSenderId: "454459962613",
+  appId: "1:454459962613:web:355fbbdd9fedf08b3241bc",
+};
+
+const app = initializeApp(firebaseConfig);
